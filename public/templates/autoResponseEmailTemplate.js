8aea5c033f29cc123132bd1e5451888973849334
rsamanthula <schinthalapudi@miraclesoft.com>
2021-02-08 17:21:36 +0530

    email template updated

--- public/templates/autoResponseEmailTemplate.js
+++ public/templates/autoResponseEmailTemplate.js
@@ -1,16 +1,23 @@
 
 emailTemplate = (details)=>{
-    return `<div style="border-style: groove;"><pre>
-    Hello <b>${details.name}</b>,
-
-    Hope you are doing great,
-
-    Thank you so much for your response, will get back to you at the earliest.
-
-    Thank you,
-    Pavan Anand Ch,
-    Software Engineer.
-    </pre></div>`
+    return `<div style="
+    border: 3px solid green;
+    padding: 10px;
+    text-align: center;
+  ">
+  <header>
+      <h1>THANK YOU!</h1>
+    </header>
+    <div style="text-align: center">
+        <i class="fa fa-check" style="color: green;
+        font-size: 195px;" aria-hidden="true"></i>
+    </div>
+    <p>Hello <b>${details.name},</b></p>
+    <p>Thanks a bunch for your valuable feekback. It means a lot, just like you do! I really appreciate you giving me a moment of your time today. Thanks for being you.</p>
+    <footer>
+		<p>Copyright ©2021 | All Rights Reserved</p>
+	</footer>
+</div>`
 }
 
 module.exports = {
